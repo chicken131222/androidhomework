a574c7fd2857630bdb5ab8fb12155edbb4c24bf6
ChickenMan <39923533+chicken131222@users.noreply.github.com>
2019-11-22 19:23:36 +0900

    Create item_list.java

---
+++ RecyclerViewEvent/item_list.java
@@ -0,0 +1,32 @@
+public class item_list {
+
+    private int id;
+    private String Itemname;
+    private String Price;
+
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int Id) {
+        this.id = Id;
+    }
+
+
+    public String getItemname() {
+        return Itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.Itemname = itemname;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        this.Price = price;
+    }
+}
